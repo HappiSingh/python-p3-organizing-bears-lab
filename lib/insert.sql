bdd20a7ffe394884ab0d6a762f849e464e8bf993
Harpreet Singh <Harpreets1201@gmail.com>
2023-07-27 01:46:43 -0400

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears
+VALUES (1, "Mr. Chocolate", 5, "M", "Brown", "Calm", 1);
+
+INSERT INTO bears
+VALUES (2, "Rowdy", 6, "M", "Brown", "Calm", 1);
+
+INSERT INTO bears
+VALUES (3, "Tabitha", 7, "F", "Brown", "Calm", 1);
+
+INSERT INTO bears
+VALUES (4, "Sergeant Brown", 7, "M", "Brown", "Calm", 1);
+
+INSERT INTO bears
+VALUES (5, "Melissa", 3, "F", "Brown", "Calm", 1);
+
+INSERT INTO bears
+VALUES (6, "Grinch", 8, "M", "Brown", "Calm", 1);
+
+INSERT INTO bears
+VALUES (7, "Wendy", 9, "F", "Brown", "Calm", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (8, NULL, 10, "M", "Brown", "Calm", 1);
